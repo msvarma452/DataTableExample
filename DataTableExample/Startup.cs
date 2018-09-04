538f29c629a60689cdcebb559ca358323a9a7f92
msvarma452 <msvarma452@gmail.com>
2018-09-04 14:27:00 +0530

    Initial commit

---
+++ DataTableExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataTableExample.Startup))]
+namespace DataTableExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
